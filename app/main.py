aeda20c2fc265954abc5e2c5caa726fb64698390
Oscar <oscar.agreda.v@gmail.com>
2025-10-25 21:00:28 -0500

    creación vm desde web

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from app.routers import slices, deployment
+from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI(title="Joyastack Orchestrator")
 
 app.include_router(slices.router)
 app.include_router(deployment.router)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+
+print(f"[INFO] Mounted static dir: {STATIC_DIR}")
+app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
--- core/models.py
+++ core/models.py
@@ -23,3 +23,29 @@ class VM(Base):
     state = Column(String(20), nullable=False, default="PENDIENTE")
     pid = Column(Integer, nullable=True)
     external_ssh_port = Column(Integer, nullable=True)
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(100), nullable=False, unique=True)
+    hash_password = Column(String(255), nullable=False)
+    role = Column(String(20), default="admin")
+
+class Worker(Base):
+    __tablename__ = "worker"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    ip = Column(String(50), nullable=False)
+    ssh_port = Column(Integer, nullable=False)
+    state = Column(String(20), default="ACTIVE")
+    capacity_cpu = Column(Integer, nullable=False)
+    ram_total = Column(Integer, nullable=False)
+
+class Image(Base):
+    __tablename__ = "image"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    path = Column(String(255), nullable=False)
+    has = Column(String(64), nullable=True)
+    size = Column(Integer, nullable=True)
+    reference_count = Column(Integer, default=0)
