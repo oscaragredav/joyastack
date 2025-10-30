3b467bf97bfc9af3ef3fa074f85bb36facbd4586
Joshe1601 <jmorillosp@pucp.edu.pe>
2025-10-30 10:16:49 -0500

    File App removed

--- app/routers/deployment.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from core.deployment_manager import deploy_slice
-
-router = APIRouter()
-
-@router.post("/slices/deploy/{slice_id}")
-async def deploy(slice_id: int):
-    result = deploy_slice(slice_id)
-    return {"slice_id": slice_id, "result": result}
